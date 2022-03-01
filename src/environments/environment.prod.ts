3bd7bcba97e7af9918b870b9f5aa854c12e2cec0
david mimay <davidmimay@gmail.com>
2022-03-01 16:27:23 +0000

    ✨ fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,27 @@
+export const environment = {
+  production: true,
+  webapp_url: 'http://localhost:4200',
+
+  stripe: {
+    restrictedKey: '',
+    subsPrice: '',
+  },
+  
+  youtube: {
+    apikey: '',
+    playlistCalm: '',
+    nextpage: '',
+  },
+  
+  // location: 'us-central1',
+  
+  firebaseConfig:  {
+    apiKey: "AIzaSyADklCWN6vR6dWmpzfxqroLdiZp1hvnXKw",
+    authDomain: "nentei.firebaseapp.com",
+    projectId: "nentei",
+    storageBucket: "nentei.appspot.com",
+    messagingSenderId: "1029765809718",
+    appId: "1:1029765809718:web:25fbcccf568c7be8a0fa97",
+    measurementId: "G-HGXQVJWB51"
+  }
+};
